642606097237c48108c97d75647bbe2e6288628b
luigi <luigidev6464@gmail.com>
2023-08-24 12:05:15 +0200

    creazione 3 pagine form solo front end

--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Form1Component } from './form1/form1.component';
 import { Form2Component } from './form2/form2.component';
+import { Form3Component } from './form3/form3.component';
 
 const routes: Routes = [
   {path: 'form1', component: Form1Component},
-  {path: 'form2', component: Form2Component}
+  {path: 'form2', component: Form2Component},
+  {path: 'form3', component: Form3Component}
 ];
 
 @NgModule({
